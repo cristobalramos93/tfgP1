1b1e543b82adacffd8067973d625b05dd63ca412
cristobalramos93 <cristora@ucm.es>
2019-12-12 00:53:31 +0100

    registro y login

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as do_logout
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as do_login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 
 def welcome(request):
@@ -15,39 +16,27 @@ def welcome(request):
         return render(request, "index.html")
     # En otro caso redireccionamos al login
     return redirect('/login')
+
+
 def register(request):
-    # Creamos el formulario de autenticación vacío
-    form = UserCreationForm()
-    if request.method == "POST":
-        # Añadimos los datos recibidos al formulario
-        form = UserCreationForm(data=request.POST)
-        # Si el formulario es válido...
-        if form.is_valid():
-
-            # Creamos la nueva cuenta de usuario
-            user = form.save()
-
-            # Si el usuario se crea correctamente
-            if user is not None:
-                # Hacemos el login manualmente
-                do_login(request, user)
-                # Y le redireccionamos a la portada
-                return redirect('/')
-
-    # Si llegamos al final renderizamos el formulario
-    return render(request, "register.html", {'form': form})
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username=username,first_name=first_name,password=password,email = email)
+        user.save();
+        return redirect("/")
+    else:
+        return render(request,'register.html')
 
 def login(request):
     # Creamos el formulario de autenticación vacío
-    form = AuthenticationForm()
     if request.method == "POST":
-        # Añadimos los datos recibidos al formulario
-        form = AuthenticationForm(data=request.POST)
-        # Si el formulario es válido...
-        if form.is_valid():
+
             # Recuperamos las credenciales validadas
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = request.POST['username']
+            password = request.POST['password']
 
             # Verificamos las credenciales del usuario
             user = authenticate(username=username, password=password)
@@ -60,7 +49,7 @@ def login(request):
                 return redirect('/')
 
     # Si llegamos al final renderizamos el formulario
-    return render(request, "login.html", {'form': form})
+    return render(request, "login.html")
 
 def logout(request):
     do_logout(request)
